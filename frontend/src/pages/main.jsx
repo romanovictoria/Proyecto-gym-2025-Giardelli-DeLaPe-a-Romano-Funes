7c3860a0815bfe700337e0ba5d4bb601a2673686
AnaRFG <2304151@ucc.edu.ar>
2025-06-06 16:57:11 -0300

    Actualizado el main solo para ruteos

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -1,31 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '@styles/index.css';
 import Login from './Login.jsx';
 import Layout from '@components/Layout.jsx';
 import App from './App.jsx';
 import Activities from '@components/Activities.jsx';
 
-function AppAndLayoutWrapper() {
-  const navigate = useNavigate();
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-
-  const logout = () => {
-    localStorage.removeItem("isLoggedIn");
-    navigate("/");
-  };
-
-  return (
-    <Layout
-      isLoggedIn={isLoggedIn}
-      logout={logout}
-      navigate={navigate}
-    >
-      <Outlet />
-    </Layout>
-  );
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -35,8 +16,7 @@ createRoot(document.getElementById('root')).render(
           <Route index element={<App />} />
           <Route path="login" element={<Login />} />
           <Route path="activities" element={<Activities />} />
-          <Route path="/" element={<AppAndLayoutWrapper />}
-          ></Route>
+          {/* <Route path="/" element={<AppAndLayoutWrapper />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
