7c76bee3ae94e73b9ec75d8db8436d4fac3581ea
Francisco <fggp09@gmail.com>
2025-06-06 14:09:35 -0300

    fix actividad > time

--- backend/model/actividad.go
+++ backend/model/actividad.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 type Actividad struct {
-	Id          int    `gorm:"primaryKey"`
-	Nombre      string `gorm:"type:varchar(350);not null"`
-	Descripcion string `gorm:"type:varchar(350);not null"`
-	Cupo        int    `gorm:"not null"`
-	Horario     string `gorm:"type:varchar(50);not null"`
+	Id          int       `gorm:"primaryKey"`
+	Nombre      string    `gorm:"type:varchar(350);not null"`
+	Descripcion string    `gorm:"type:varchar(350);not null"`
+	Cupo        int       `gorm:"not null"`
+	Horario     time.Time `gorm:"type:time;not null"`
 
 	Categoria   Categoria `gorm:"foreignkey:CategoriaId"`
 	CategoriaId int
