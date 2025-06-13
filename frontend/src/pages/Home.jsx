1f3507a1d11862db120af1995d3cb099103558e2
romanovictoria <vicctoriarom@gmail.com>
2025-06-12 21:52:17 -0300

    comenzando pagina home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => {
 }
 
 
-
 export default Home;
 
 
--- frontend/src/pages/Layout/PrivateLayout.jsx
+++ frontend/src/pages/Layout/PrivateLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const PrivateLayout = () => {
     return (
         <div>
-            //TODO: Taca poner un componente navbarprivate que permita navegar entre las paginas privadas
+            {/*//TODO: Taca poner un componente navbarprivate que permita navegar entre las paginas privadas  */}
             <main>
                 <Outlet />
             </main>
