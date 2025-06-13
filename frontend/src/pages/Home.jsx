d088ec32244c2d8f37ba73dbf0da5755bab841fc
Francisco <fggp09@gmail.com>
2025-06-13 04:05:16 -0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,63 +1,27 @@
 import "@styles/Home.css";
-import React, { useEffect, useState } from "react";
-import { useApiRequest } from "../hooks/useApiRequest";
-
+import { useState, useEffect } from "react";
 const Home = () => {
   const [actividades, setActividades] = useState([]);
-  const {
-    data: loginResponse,
-    error,
-    request: fetchActividad,
-  } = useApiRequest();
 
   useEffect(() => {
     // Verificar si el usuario es un administrador al cargar la página
     const isAdmin = localStorage.getItem("isAdmin");
-    if (isAdmin === "true") {
+    if (isAdmin === "false") {
       console.log("Hola");
       fetchActividad();
-    } else {
-      //redirigir a otra página o mostrar un mensaje de acceso denegado
     }
   }, []);
 
-  /* fetchActividad = async () => {
-    try {
-      // Realizar la solicitud para obtener las actividades de todos los usuarios
-      const response = await fetch("http://localhost:8080/actividad");
-      const data = await response.json();
-
-      // Almacenar las actividades en el estado
-      console.log(data.actividades);
-      setActividades(data.actividades);
-    } catch (error) {
-      console.log("Error al obtener las actividades:", error);
-    }
-  };
- */
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    const body = JSON.stringify({
-      nombre: nombre,
-      descripcion: descripcion,
-      id: id,
-      categoriaId: categoriaId,
-      cupo: cupo,
-      horario: horario,
-    });
-
-    // Hacer la petición login y obtener la respuesta
-    await fetchActividad("http://localhost:8080/actividad", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body,
-    });
-    console.log(body);
-  };
-
+ const fetchActividad = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/actividad");
+    const data = await response.json();
+    setActividades(data);
+  } catch (error) {
+    console.error("Error al obtener actividades:", error);
+  }
+};
+  console.log(actividades);
   return (
     <div>
       <div>
@@ -74,7 +38,7 @@ const Home = () => {
               {/* Mostrar los detalles de la actividad */}
               <p>Titulo: {actividad.nombre}</p>
               <p>Horario: {actividad.horario}</p>
-              <p>Profesor: {actividad.usuario.nombre}</p>
+              {/* <p>Profesor: {actividad.usuario.nombre}</p> */}
             </li>
           ))}
         </ul>
